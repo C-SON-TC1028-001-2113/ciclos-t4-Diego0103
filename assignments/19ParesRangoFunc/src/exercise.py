119a435701c24e3901253f8b27fd4c6c31bd7fdf
Diego0103 <A01254073@itesm.mx>
2021-09-05 21:07:34 +0000

    19ParesRangoFunc

--- assignments/19ParesRangoFunc/src/exercise.py
+++ assignments/19ParesRangoFunc/src/exercise.py
@@ -1,6 +1,26 @@
+def pares(num1,num2):
+    if num1<num2 and num1%2==0:
+        for i in range(num1,num2+1,2):
+            print(i)
+    elif num1<num2 and num1%2!=0:
+        for i in range(num1+1,num2+1,2):
+            print(i)
+    elif num1>num2 and num2%2==0:
+        for i in range(num2,num1+1,2):
+            print(i)
+    elif num1>num2 and num2%2!=0:
+        for i in range(num2+1,num1+1,2):
+            print(i)
+            
 
 def main():
-
+    valor1=int(input("Valor 1: "))
+    valor2=int(input("Valor 2: "))
+    if valor1==valor2 and valor1%2!=0:
+        print("No hay pares")
+    elif valor1>0 and valor2>0:
+        pares(valor1,valor2)
+    
 
 if __name__=='__main__':
     main()
